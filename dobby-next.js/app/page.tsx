d5bd5d6be0e4000261584fbb2e2d3734ff52342b
Luka Volarevic <lvolarevic@croz.net>
2025-10-12 00:28:04 +0200

    Added initial phase, shadcn installation, supabase and auth routes, theme provider and switcher

--- dobby-next.js/app/page.tsx
+++ dobby-next.js/app/page.tsx
@@ -14,14 +14,14 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Initialized {" "}
+            Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              Dobby
+              app/page.tsx
             </code>
             .
           </li>
           <li className="tracking-[-.01em]">
-            You can start developing.
+            Save and see your changes instantly.
           </li>
         </ol>
 
@@ -39,7 +39,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deployment
+            Deploy now
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
@@ -47,7 +47,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read the docs
+            Read our docs
           </a>
         </div>
       </main>
